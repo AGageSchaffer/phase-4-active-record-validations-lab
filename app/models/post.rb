7fd17d6e01df69a44547b4a4452f510223a1387f
AustinSchaffer <austinschaffer@gmail.com>
2023-02-22 10:33:57 -0600

    commit tree 3b8304047f7658bcbe1071a4cc3de93455eca263

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {
+        in: %w(Fiction Non-Fiction)
+    }
+    validate :title_is_clickbate
+
+    def title_is_clickbate
+        if (title.present?)
+            if (!title.match("Won't Believe" || "Secret" || "Top #{number}" || "Guess"))
+            errors.add(:title, "Title isn't clickbate-y")
+            end
+        end
+    end
+
 end
